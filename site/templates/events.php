1a70f3c2c96e875f1e5f1e90c36f8106d6a6d0e8
Christian Reichel <chrstn.rchl@gmail.com>
2019-09-28 11:44:12 +0200

    ✨ Add events template, close #9

---
+++ site/templates/events.php
@@ -0,0 +1,33 @@
+<?php snippet('header') ?>
+
+<main>
+  <header class="intro">
+    <h1><?= $page->title() ?></h1>
+  </header>
+  <div class="text">
+    <?= $page->text()->kt() ?>
+  </div>
+  <section class="events">
+    <?php if (page('events')->upcoming() == NULL): ?>
+    <article class="empty">
+      <h3><?= page('events')->emptytitle(); ?></h3>
+      <p><?= page('events')->emptytext(); ?></p>
+    </article>
+    <?php else: ?>
+    <?php foreach (page('events')->upcoming() as $event): ?>
+    <article class="event">
+      <header class="event-header">
+        <div>
+          <h3><time><?= $event['from']->toDate('d.m H:i') ?></time> - <time><?= $event['to']->toDate('d.m H:i') ?></time></h3>
+          <p><?= $event['event']->title() ?> @ <?= $event['location'] ?></p>
+          
+          <a class='ical' href="<?= page('ical')->url() ?>/id:<?= $event['id'] ?>">🗓</a>
+        </div>
+      </header>
+    </article>
+    <?php endforeach ?>
+    <?php endif ?>
+  </section>
+</main>
+
+<?php snippet('footer') ?>
